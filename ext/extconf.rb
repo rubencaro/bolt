cdd5fbe9d4e5e96ccac8cd13b13ed3d3d479d9dc
Rubén Caro <ruben@elpulgardelpanda.com>
2014-09-24 08:46:37 +0200

    Add fake extension compilation. Try 1.

---
+++ ext/extconf.rb
@@ -0,0 +1,14 @@
+
+=begin
+
+  Fake extension building
+
+  This allows for putting our bash scripts on the path, letting rubygems
+  think we are compiling something...
+
+  To fool rubygems we need a fake Makefile which can run `make all` and
+  `make install`. Then we also need to place the fake `*.so` library, result of
+  the fake Makefile.
+
+=end
+
